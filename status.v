8a237d30618273fd063d84e29d94e3ce43e3feda
yasinalperbingul <55395610+yasinalperbingul@users.noreply.github.com>
2022-06-02 20:52:37 +0300

    Fixed issues

--- status.v
+++ status.v
@@ -3,7 +3,9 @@ input [31:0] alu_result;
 input alu_zero;
 output n_out, z_out, v_out;
 
+assign z_out = ~(|alu_result);
 assign n_out = alu_result[31];
-assign z_out = alu_zero;
+
+
 //v_out oluştur
 endmodule
